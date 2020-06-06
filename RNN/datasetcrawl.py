0c8d1410f50f311fe25bf49bc95556e8757eee6c
seungjae6874 <qkrtmdwo1@gmail.com>
2020-06-06 20:47:42 +0900

    crawling libre

--- RNN/datasetcrawl.py
+++ RNN/datasetcrawl.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
+from goodbyecaptcha.solver import Solver
 import re, csv
 from bs4 import BeautifulSoup
 import openpyxl
 import time
-import numpy as np
-import unittest
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -24,7 +23,19 @@ driver.maximize_window()
 driver.implicitly_wait(20)
 
 driver.get('https://www.libreview.com')
+#
+proxy = "127.0.0.1:1000"
+auth_details = {"username": "peanut0223@nate.com", "password": "Jongsul123"}
+args = ["--timeout 5"]
+options = {"ignoreHTTPSErrors": True, "args": args}
+client = Solver(
+    # With Proxy
+    'https://www.libreview.com', sitekey, options=options, proxy=proxy, proxy_auth=auth_details
+    # Without Proxy
+    # pageurl, sitekey, options=options
+)
 
+#
 
 
 
@@ -60,110 +71,14 @@ driver.implicitly_wait(20)
 driver.find_element_by_id('exportData-button').click()
 
 #로봇이 아닙니다 체크박스 해결하기
-'''
+
 mainwindow = driver.current_window_handle
 
 frame = driver.find_element_by_tag_name("iframe")
 driver.switch_to.frame(frame)
 driver.find_element_by_id("recaptcha-anchor").click()
 driver.switch_to.window(mainwindow)
-'''
 
-def write_stat(loops, time):
-	with open('stat.csv', 'a', newline='') as csvfile:
-		spamwriter = csv.writer(csvfile, delimiter=',',
-								quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		spamwriter.writerow([loops, time])  	 
-	
-def check_exists_by_xpath(xpath):
-    try:
-        driver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
-	
-def wait_between(a,b):
-	rand=uniform(a, b) 
-	sleep(rand)
- 
-def dimention(driver): 
-	d = int(driver.find_element_by_xpath('//div[@id="rc-imageselect-target"]/table').get_attribute("class")[-1]);
-	return d if d else 3  # dimention is 3 by default
-	
-# ***** main procedure to identify and submit picture solution	
-def solve_images(driver):	
-	WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID ,"rc-imageselect-target"))
-        ) 		
-	dim = dimention(driver)	
-	# ****************** check if there is a clicked tile ******************
-	if check_exists_by_xpath('//div[@id="rc-imageselect-target"]/table/tbody/tr/td[@class="rc-imageselect-tileselected"]'):
-		rand2 = 0
-	else:  
-		rand2 = 1 
-
-	# wait before click on tiles 	
-	wait_between(0.5, 1.0)		 
-	# ****************** click on a tile ****************** 
-	tile1 = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH ,   '//div[@id="rc-imageselect-target"]/table/tbody/tr[{0}]/td[{1}]'.format(randint(1, dim), randint(1, dim )))) 
-		)   
-	tile1.click() 
-	if (rand2):
-		try:
-			driver.find_element_by_xpath('//div[@id="rc-imageselect-target"]/table/tbody/tr[{0}]/td[{1}]'.format(randint(1, dim), randint(1, dim))).click()
-		except NoSuchElementException:          		
-		    print('\n\r No Such Element Exception for finding 2nd tile')
-   
-	 
-	#****************** click on submit buttion ****************** 
-	driver.find_element_by_id("recaptcha-verify-button").click()
-
-
-
-start = time()
-mainWin = driver.current_window_handle  
-
-# move the driver to the first iFrame 
-driver.switch_to_frame(driver.find_elements_by_tag_name("iframe")[0])
-
-# *************  locate CheckBox  **************
-CheckBox = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID ,"recaptcha-anchor"))
-        ) 
-
-# *************  click CheckBox  ***************
-wait_between(0.5, 0.7)  
-# making click on captcha CheckBox 
-CheckBox.click() 
- 
-#***************** back to main window **************************************
-driver.switch_to_window  
-
-wait_between(2.0, 2.5) 
-
-# ************ switch to the second iframe by tag name ******************
-driver.switch_to_frame(driver.find_elements_by_tag_name("iframe")[0]) 
-# ************ switch to the second iframe by tag name ******************
-#driver.switch_to_frame(driver.find_elements_by_tag_name("iframe")[1])  
-i=1
-while i<130:
-    print('\n\r{0}-th loop'.format(i))
-    driver.switch_to_window
-    #WebDriverWait(driver,10).until(
-    #    EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME , 'iframe'))
-    #    )
-    wait_between(1.0, 2.0)
-    if check_exists_by_xpath('//span[@aria-checked="true"]'):
-        import winsound
-        winsound.Beep(400,1500)
-        write_stat(i, round(time()-start)-1)
-        break
-    wait_between(0.3, 1.5)
-    driver.implicitly_wait(20)
-    #driver.switch_to_frame(driver.find_elements_by_tag_name("iframe")[1])
-    solve_images(driver)
-    i=i+1
 
 
 
