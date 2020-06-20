60658c4e7f928cceb2ffdc1cee70dc94b2bbe4a3
seungjae6874 <qkrtmdwo1@gmail.com>
2020-06-20 17:58:16 +0900

    food app

---
+++ APP/Diabete/app/src/main/java/com/example/diabete/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.diabete;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button search;
+    WebView webView;
+    String source;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        search = findViewById(R.id.web);
+        webView = findViewById(R.id.webView);
+
+        //WebVew 자바스크립트 활성화
+        webView.getSettings().setJavaScriptEnabled(true);
+        WebSettings set = webView.getSettings();
+        set.setLoadWithOverviewMode(true);
+        set.setUseWideViewPort(true);
+        // 자바스크립트인터페이스 연결
+        // 이걸 통해 자바스크립트 내에서 자바함수에 접근할 수 있음.
+        webView.addJavascriptInterface(new MyJavascriptInterface(), "Android");
+        // 페이지가 모두 로드되었을 때, 작업 정의
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 자바스크립트 인터페이스로 연결되어 있는 getHTML를 실행
+                // 자바스크립트 기본 메소드로 html 소스를 통째로 지정해서 인자로 넘김
+                view.loadUrl("javascript:window.Android.getHtml(document.getElementsByTagName('body')[0].innerHTML);");
+            }
+        });
+
+        //지정한 URL을 웹 뷰로 접근하기
+        webView.loadUrl("https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/calorieDic.do?menu_no=3072&menu_grp=MENU_NEW03");
+    }
+
+    public class MyJavascriptInterface {
+        @JavascriptInterface
+        public void getHtml(String html) {
+            //위 자바스크립트가 호출되면 여기로 html이 반환됨
+            source = html;
+        }
+    }
+}
+
+
